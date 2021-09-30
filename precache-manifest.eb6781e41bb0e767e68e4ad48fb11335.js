dffbb40f0fdb00fcc5d6da1ee7e3d9b2f2698f24
Umair Akhtar <umair.akhtar@airtel.com>
2021-10-01 03:06:27 +0530

    Updates

---
+++ precache-manifest.eb6781e41bb0e767e68e4ad48fb11335.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f301513e26d8d85c55f6f6af07a9282",
+    "url": "/starWarsMeetsAnalytics/index.html"
+  },
+  {
+    "revision": "087d8b5a72fc1fde65c8",
+    "url": "/starWarsMeetsAnalytics/static/css/main.608958ff.chunk.css"
+  },
+  {
+    "revision": "b4598d884556e2e98fda",
+    "url": "/starWarsMeetsAnalytics/static/js/2.14bfbf56.chunk.js"
+  },
+  {
+    "revision": "292186139ac9474ee24a17d731964152",
+    "url": "/starWarsMeetsAnalytics/static/js/2.14bfbf56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087d8b5a72fc1fde65c8",
+    "url": "/starWarsMeetsAnalytics/static/js/main.6d9275b5.chunk.js"
+  },
+  {
+    "revision": "bc99d861da642666a77b",
+    "url": "/starWarsMeetsAnalytics/static/js/runtime-main.3d04ba74.js"
+  },
+  {
+    "revision": "71c14ba9a88065c99b78bc3a0408719c",
+    "url": "/starWarsMeetsAnalytics/static/media/planet.71c14ba9.jpg"
+  },
+  {
+    "revision": "a1288c35e3935b283fd2408371190e74",
+    "url": "/starWarsMeetsAnalytics/static/media/yoda_transparent.a1288c35.png"
+  }
+]);
